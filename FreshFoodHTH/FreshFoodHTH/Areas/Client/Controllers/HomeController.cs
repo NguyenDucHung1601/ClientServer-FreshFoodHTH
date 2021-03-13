70169834de75e380c9b281ab226cb5ca28b5f905
Nguyễn Đức Hưng <48803420+NguyenDucHung1601@users.noreply.github.com>
2021-03-14 00:59:07 +0700

    Home Client

--- FreshFoodHTH/FreshFoodHTH/Areas/Client/Controllers/HomeController.cs
+++ FreshFoodHTH/FreshFoodHTH/Areas/Client/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FreshFoodHTH.Models.EF;
+using FreshFoodHTH.Models.EFplus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,33 @@ namespace FreshFoodHTH.Areas.Client.Controllers
 {
     public class HomeController : Controller
     {
+        FreshFoodDBContext db = new FreshFoodDBContext();
+
         // GET: Client/Home
-        public ActionResult Index()
+        public ActionResult Index(string searching)
         {
-            return View();
+            IEnumerable<SanPham> list;
+            ViewBag.Searching = searching;
+            if (searching != null)
+                list = db.SanPhams.Where(x => x.Ten.Contains(searching) || x.TheLoai.Ten.Contains(searching)).ToList();
+            else
+                list = db.SanPhams.ToList();
+            return View(list);
+        }
+
+        public ActionResult CategoryShow()
+        {
+            return PartialView(db.TheLoais.ToList());
+        }
+
+        public ActionResult CategoryShowImage()
+        {
+            return PartialView(db.TheLoais.ToList());
+        }
+
+        public ActionResult ListCategoryShow()
+        {
+            return PartialView(db.TheLoais.ToList());
         }
     }
 }
