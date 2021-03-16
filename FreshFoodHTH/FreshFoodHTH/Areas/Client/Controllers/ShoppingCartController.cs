70ec741f06be0e503cd95cc69bf533ef67bde45a
Nguyễn Đức Hưng <48803420+NguyenDucHung1601@users.noreply.github.com>
2021-03-16 14:45:43 +0700

    cm

---
+++ FreshFoodHTH/FreshFoodHTH/Areas/Client/Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreshFoodHTH.Areas.Client.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: Client/ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
